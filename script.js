b2d68622f773495576bd8267dca19a0968022984
Kanai-06 <153616932+Kanai-06@users.noreply.github.com>
2024-09-06 19:54:47 +0200

    Adaptation for touchscreen attempt 6

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+let onTouchLeaveEvents = [];
+let onTouchEnterEvents = [];
+const onTouchEnter = function (selector, fn) {
+    onTouchEnterEvents.push([selector, fn]);
+    return function () {
+        onTouchEnterEvents.slice().map(function (e, i) {
+            if (e[0] === selector && e[1] === fn) {
+                onTouchEnterEvents.splice(1, i);
+            }
+        });
+    };
+};
+
+const onTouchLeave = function (selector, fn) {
+    onTouchLeaveEvents.push([selector, fn]);
+    return function () {
+        onTouchLeaveEvents.slice().map(function (e, i) {
+            if (e[0] === selector && e[1] === fn) {
+                onTouchLeaveEvents.splice(1, i);
+            }
+        });
+    };
+};
+
+let lastTouchLeave;
+let lastTouchEnter;
+document.addEventListener('touchmove', function (e) {
+    var el = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+    if (!el) return;
+  
+  onTouchLeaveEvents.map((event) => {
+    if (el!=lastTouchEnter && lastTouchEnter && lastTouchEnter.matches(event[0])) {
+      
+        
+            if (lastTouchEnter !== lastTouchLeave ) {
+                event[1](lastTouchEnter, e);
+        lastTouchLeave = lastTouchEnter;
+        lastTouchEnter = null
+            }
+            
+        }
+    });
+  
+    onTouchEnterEvents.map((event) => {
+        if (el.matches(event[0]) && el!==lastTouchEnter) {
+            lastTouchEnter = el;
+      lastTouchLeave = null;
+      event[1](el, e);
+     }
+    });
+
+});
+
+onTouchEnter('.area',function(el,e){
+  el.classList.add('hover')
+})
+onTouchLeave('.area',function(el,e){
+  el.classList.remove('hover')
+})
+
+
 function draw(numberBoxes){
     const drawingBox = document.querySelector("#drawingBox");
 
@@ -21,14 +82,9 @@ function draw(numberBoxes){
     });
 
     document.querySelectorAll("#drawingBox div").forEach((child) => {
-        child.addEventListener("touchenter",() => {
+        onTouchEnter(child, function(element,event){
             child.style.background = "black";
-        });
-
-        child.addEventListener("touchenter",(e) => {
-            child.style.background = "black";
-            e.preventDefault();
-        });
+        })
     });
 }
 
